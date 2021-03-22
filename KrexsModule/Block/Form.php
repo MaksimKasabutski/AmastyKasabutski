9224930089e6ed1f334f229bc9df1f3f05f9e45c
MaksimKasabutski <kmv5125@gmail.com>
2021-03-22 12:25:30 +0300

    Make some coding

--- KrexsModule/Block/Form.php
+++ KrexsModule/Block/Form.php
@@ -3,8 +3,35 @@
 namespace Amasty\KrexsModule\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Form extends Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
 
+    public function __construct(
+        Template\Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    )
+    {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+    public function getQtyFieldShowStatus(): bool
+    {
+        if($this->scopeConfig->getValue('krexs_config/general/show_qty')) {
+            return true;
+        } else return false;
+    }
+
+    public function getQtyValue(): string
+    {
+       $value = $this->scopeConfig->getValue('krexs_config/general/qty_default_value');
+       return $value ? $value : '';
+    }
 }
--- KrexsModule/Controller/Index/Index.php
+++ KrexsModule/Controller/Index/Index.php
@@ -3,13 +3,53 @@
 namespace Amasty\KrexsModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class Index extends Action
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    private $checkoutSession;
+
+    private $productRepository;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        CheckoutSession $checkoutSession,
+        ProductRepositoryInterface $productRepository,
+        Context $context
+)
+    {
+        $this->checkoutSession=$checkoutSession;
+        $this->scopeConfig=$scopeConfig;
+        $this->productRepository=$productRepository;
+        parent::__construct($context);
+    }
 
     public function execute()
     {
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        /*$quote = $this->checkoutSession->getQuote();
+
+        if(!$quote->getId()) {
+            $quote->save();
+        }
+
+        $product = $this->productRepository->get('24-MB01');
+
+        $quote->addProduct($product, 2);
+        $quote->save();*/
+
+        if($this->scopeConfig->isSetFlag('krexs_config/general/enabled')) {
+            return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        } else {
+            die('Модуль не включен');
+        }
     }
 }
